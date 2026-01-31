0ccdb364d22dfb1e5ab2c4259abd1c04fe342263
koffeejava <aidenspurger572@gmail.com>
2026-01-30 21:26:10 -0700

    updated error handler. added it to all functions.

--- src/test.py
+++
@@ -1,6 +0,0 @@
-import macaronipm
-
-macaronipm.misc.setToken("6e5035471364acbbdb523dd05e903ccd48c3a60ea6d5ec14c485a833dccf555f")
-macaronipm.misc.setProject("528471029")
-
-print(macaronipm.project.getThumbnail())
